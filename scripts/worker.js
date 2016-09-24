5076045b8b6dcfc21ab45c4bbdaeee6d81f2c598
Huan Chen <hchen.fred@gmail.com>
2016-09-24 08:47:58 -0700

    feat: use web worker to remove jank

---
+++ scripts/worker.js
@@ -0,0 +1,32 @@
+importScripts('imageManips.js');
+
+this.onmessage = function(e) {
+  var imageData = e.data.imageData;
+  var type = e.data.type;
+  console.log(e.data.type);
+
+  try {
+    console.log("here");
+    length = imageData.data.length / 4;
+    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+      r = imageData.data[i * 4 + 0];
+      g = imageData.data[i * 4 + 1];
+      b = imageData.data[i * 4 + 2];
+      a = imageData.data[i * 4 + 3];
+      pixel = manipulate(type, r, g, b, a);
+      imageData.data[i * 4 + 0] = pixel[0];
+      imageData.data[i * 4 + 1] = pixel[1];
+      imageData.data[i * 4 + 2] = pixel[2];
+      imageData.data[i * 4 + 3] = pixel[3];
+    }
+    //send message from worker to main
+    this.postMessage(imageData);
+  } catch (e) {
+    function ManipulationException(message) {
+      this.name = "ManipulationException";
+      this.message = message;
+    };
+    throw new ManipulationException('Image manipulation error');
+    postMessage(undefined);
+  }
+}
